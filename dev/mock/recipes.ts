daa150ed31ed263b7c3e8ea5e9c6f9de03d1e2d6
Rosco <rosco9awj@gmail.com>
2016-03-10 14:31:29 -0500

    Created data sources

--- dev/mock/recipes.ts
+++ dev/mock/recipes.ts
@@ -1,3 +1,11 @@
+import {Recipe} from "../shared/recipe";
+import {Ingredient} from "../shared/ingredient";
 export let RECIPES: Recipe[] = [
-
+	new Recipe(
+		'Wiener Schnitzel', 
+		'Delicious',
+		'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/WienerSchnitzel.jpg',[
+			new Ingredient('Pork Meat', 1),
+			new Ingredient('salad', 2)
+		])
 ];
