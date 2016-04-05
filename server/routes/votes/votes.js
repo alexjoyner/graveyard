b2cb03b76f22fef91fc3ad319ef9b058e5a58c5d
Rosco <rosco9awj@gmail.com>
2016-04-05 18:25:45 -0400

    v1.0.0 Alpha.6

--- server/routes/votes/votes.js
+++ server/routes/votes/votes.js
@@ -60,36 +60,30 @@ router.post('/issue', jwt_verify, function(req, res) {
             if(vote){ // Step 1.YES
                 res.status(500).send('Vote already cast').endl;
             }else{ // Step 1.NO
-                var shouldReturn = false; // Step 1.2
                 var newVote = new votes();
                 newVote.itemId = issueId;
                 newVote.itemType = 1;
                 newVote.userId = profile.id;
                 newVote.voteType = voteType;
-                console.log(profile, newVote);
                 newVote.save(function(err){
                     if(err) throw err;
-                    if(shouldReturn){
-                        res.status(200).send(voteType + ' successful').end();
-                    }else{
-                        shouldReturn = !shouldReturn;
-                    }
-                })
-                issues // Step 1.4
-                    .findOneAndUpdate(query, incOrder)
-                    .select('ups downs')
-                    .exec(function(err, issue) {
-                        if (err) throw err;
-                        if (!issue) {
-                            res.status(500).send('No Issue Found');
-                        } else {
-                            if(shouldReturn){
-                                res.status(200).send(voteType + ' successful').end();
-                            }else{
-                                shouldReturn = !shouldReturn;
+                    issues // Step 1.4
+                        .findOneAndUpdate(query, incOrder)
+                        .select('ups downs')
+                        .exec(function(err, issue) {
+                            if (err) throw err;
+                            if (!issue) {
+                                res.status(500).send('No Issue Found');
+                            } else {
+                                if(shouldReturn){
+                                    res.status(200).send(voteType + ' successful').end();
+                                }else{
+                                    shouldReturn = !shouldReturn;
+                                }
                             }
-                        }
-                    });
+                        });
+                })
+                
 
             }
         })
@@ -98,71 +92,127 @@ router.post('/issue', jwt_verify, function(req, res) {
 // Post vote for point
 router.post('/point', jwt_verify, function(req, res) {
     var voteType = req.body.voteType, // 'upvote' or 'downvote'
-        pointId = req.body.pointId;
+        pointId = req.body.pointId,
+        profile = req.decoded;
+
     // Build order
     var query = {
         '_id': pointId
     };
     var incOrder;
+    var voteType;
     if (voteType === 'upvote') {
+        voteType = 1;
         incOrder = {
             '$inc': {
                 'ups': 1
             }
         };
     } else {
+        voteType = 2;
         incOrder = {
             '$inc': {
                 'downs': 1
             }
         };
     }
-    points
-        .findOneAndUpdate(query, incOrder)
-        .select('ups downs')
-        .exec(function(err, issue) {
-            if (err) throw err;
-            if (!issue) {
-                res.status(500).send('No Issue Found');
-            } else {
-                res.status(200).send(voteType + ' successful').end();
+    votes
+        .findOne({ // Step 1
+            $and: [{
+                'userId': profile.id
+            },{
+                'itemId': pointId
+            }]
+        })
+        .exec(function(err, vote){
+            if(err) throw err;
+            if(vote){ // Step 1.YES
+                res.status(500).send('Vote already cast').endl;
+            }else{ // Step 1.NO
+                var newVote = new votes();
+                newVote.itemId = pointId;
+                newVote.itemType = 2;
+                newVote.userId = profile.id;
+                newVote.voteType = voteType;
+                console.log('New Vote', newVote);
+                newVote.save(function(err){
+                    if(err) throw err;
+                    points
+                        .findOneAndUpdate(query, incOrder)
+                        .select('ups downs')
+                        .exec(function(err, issue) {
+                            if (err) throw err;
+                            if (!issue) {
+                                res.status(500).send('No Issue Found');
+                            } else {
+                                res.status(200).send(voteType + ' successful').end();
+                            }
+                        });
+                })
+
             }
-        });
+        })
 });
 // Post vote for support
 router.post('/support', jwt_verify, function(req, res) {
     var voteType = req.body.voteType, // 'upvote' or 'downvote'
-        supportId = req.body.supportId;
+        supportId = req.body.supportId,
+        profile = req.decoded;
     // Build order
     var query = {
         '_id': supportId
     };
     var incOrder;
+    var voteType;
     if (voteType === 'upvote') {
+        voteType = 1;
         incOrder = {
             '$inc': {
                 'ups': 1
             }
         };
     } else {
+        voteType = 2;
         incOrder = {
             '$inc': {
                 'downs': 1
             }
         };
     }
-    console.log('Query: ', query);
-    console.log('incOrder: ', incOrder);
-    supports
-        .findOneAndUpdate(query, incOrder)
-        .select('ups downs')
-        .exec(function(err, support) {
-            if (err) throw err;
-            if (!support) {
-                res.status(500).send('No Issue Found');
-            } else {
-                res.status(200).send(voteType + ' successful').end();
+    votes
+        .findOne({ // Step 1
+            $and: [{
+                'userId': profile.id
+            },{
+                'itemId': supportId
+            }]
+        })
+        .exec(function(err, vote){
+            if(err) throw err;
+            if(vote){ // Step 1.YES
+                res.status(500).send('Vote already cast').endl;
+            }else{ // Step 1.NO
+                var newVote = new votes();
+                newVote.itemId = supportId;
+                newVote.itemType = 3;
+                newVote.userId = profile.id;
+                newVote.voteType = voteType;
+                console.log('New Vote: ', newVote);
+                newVote.save(function(err){
+                    if(err) throw err;
+                    supports
+                        .findOneAndUpdate(query, incOrder)
+                        .select('ups downs')
+                        .exec(function(err, support) {
+                            if (err) throw err;
+                            if (!support) {
+                                res.status(500).send('No Issue Found');
+                            } else {
+                                res.status(200).send(voteType + ' successful').end();
+                            }
+                        });
+                })
             }
-        });
+        })
 });
 module.exports = router;
