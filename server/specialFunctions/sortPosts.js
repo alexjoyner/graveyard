6b2c07ceead0d84d40ce19bd56fac3070cb87976
Rosco <rosco9awj@gmail.com>
2016-05-31 18:13:53 -0400

    Changed up the sorting algorith to use a reddit style hot sort algorythm on the front page

--- server/specialFunctions/sortPosts.js
+++ server/specialFunctions/sortPosts.js
@@ -1,5 +1,10 @@
+var decay = require('decay')
+  , hotScore = decay.redditHot();
+
 module.exports = function(posts){
 	return posts.sort(function(postA, postB){
-		return postB.score - postA.score
+		var aScore = hotScore(postA.upVotes, postA.dnVotes, postA.created_at);
+		var bScore = hotScore(postB.upVotes, postB.dnVotes, postB.created_at);
+		return bScore - aScore
 	})
 }
