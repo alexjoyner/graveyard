ac5b2b1b9aea1c0e1bd19e032b913512e6bdb0aa
Rosco <rosco9awj@gmail.com>
2016-08-30 23:43:21 -0400

    finished writing our tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import CommentReducer from './comments';
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  comments: CommentReducer
 });
 
 export default rootReducer;
