5e4ad55f5d658c86e5a90aaa3337510d6152750e
Rosco <rosco9awj@gmail.com>
2016-09-10 21:15:52 -0400

    no message

--- alarm.py
+++ alarm.py
@@ -14,6 +14,7 @@ GPIO.setup(heaterBlanketPin, GPIO.OUT)
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 print("Right now the time is: " + time.ctime())
 response = raw_input("Please input the time for the alarm in format HHMM: \n")
+GPIO.output(heaterBlanketPin, False)
 
 print("Alarm has been set for %s hrs" % response)
 
