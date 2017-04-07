cc2dcc68e71ee9b4a7b2249596296aa0479c17a5
Rosco <rosco9awj@gmail.com>
2017-04-07 06:19:11 -0400

    changed variable names to make comments redundant

--- client/js/index.js
+++ client/js/index.js
@@ -7,19 +7,15 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import routes from './routes';
 
-/* Creating our store with a promise middleware injected so that
-* we can Mock Up data for our test environments.  This is for testing purposes
-* and as far as our regular development is concerned, we are just doing
-* const store = createstore(reducers);*/
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-const store = createStoreWithMiddleware(reducers);
+const createStoreWithPromises = applyMiddleware(promise)(createStore);
+const storeWithPromises = createStoreWithPromises(reducers);
 
 /* This is the root of our app where everything starts.
 * ReactDOM calls the render function to start our app*/
 ReactDOM.render(
 	/* The Provider element makes our store available to all of our
 	* containers.  It becomes our root component of the app.*/
-  <Provider store={store}>
+  <Provider store={storeWithPromises}>
       <Router history={hashHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.xx-appContainer'));
