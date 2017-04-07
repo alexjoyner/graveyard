b37e6a91749ac79ba5b75261a7cf190bcfd1c918
Rosco <rosco9awj@gmail.com>
2017-04-07 06:23:53 -0400

    - changed variable names to clean up comments

--- client/js/index.js
+++ client/js/index.js
@@ -10,12 +10,12 @@ import routes from './routes';
 const createStoreWithPromises = applyMiddleware(promise)(createStore);
 const storeWithPromises = createStoreWithPromises(reducers);
 
-/* This is the root of our app where everything starts.
-* ReactDOM calls the render function to start our app*/
-ReactDOM.render(
-	/* The Provider element makes our store available to all of our
-	* containers.  It becomes our root component of the app.*/
-  <Provider store={storeWithPromises}>
-      <Router history={hashHistory} routes={routes}/>
-  </Provider>
-  , document.querySelector('.xx-appContainer'));
+const renderAppWithReact = ReactDOM.render;
+const baseProviderElement = (
+	<Provider store={storeWithPromises}>
+		<Router history={hashHistory} routes={routes}/>
+	</Provider>);
+const baseIndexHtmlAppComponent = document.querySelector('.xx-appContainer');
+
+
+renderAppWithReact(baseProviderElement, baseIndexHtmlAppComponent);
