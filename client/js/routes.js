24814d3d7412cb5b74e4f720cfb9e12467b8c7a1
Rosco <rosco9awj@gmail.com>
2017-04-07 06:14:08 -0400

    removed unnecessary comments

--- client/js/routes.js
+++ client/js/routes.js
@@ -1,22 +1,11 @@
-/* Base imports for routes*/
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-
-/* Import the _app-root app wrapper component
-* 		this is the _app-root component that
-* 		everything will root from through
-* 		calling {this.props.children}
-*/
-
-/* Import _app-root container routes */
 import {Demo} from '../components/containers/Demo/index';
 
-// IndexRoute:
-// If the route is '/', show our home page
+
+
 export default(
 	<Route path='/'>
-		{/*We need an index route that will be
-		our home container at / */}
 		<IndexRoute component={Demo} />
 	</Route>
 )
