7ea75601cc9e2ac295e5fbd93fc6882f73281ff5
Rosco <rosco9awj@gmail.com>
2017-04-07 19:34:11 -0400

    cleaning more unnecessary comments

--- client/js/actions/index.js
+++ client/js/actions/index.js
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
 
-/*
-* We keep our action types in a variable for the purposes of
-* making sure that our action values stay consistent throughout our app
-* and we don't mess up and have what we called the action in our reducer switch
-* be the same thing that we named our action type in our action creator return.
-* NOTE: For consistency, make sure the variable name here is the same thing as
-* the string name value of the action name.
-*  Meaning	\/ \/ \/ \/ \/===\/ \/ \/ \/ \/ \/ */
-export const POST_PREORDER = 'POST_PREORDER';
+const POST_PREORDER = 'POST_PREORDER';
 
 const ROOT_URL = 'http://localhost:8080';
 
@@ -17,7 +9,7 @@ const ROOT_URL = 'http://localhost:8080';
 * This function preOrderApp is called an action creator,
 * an action creator is a function that returns an action
 * */
-export const preOrderApp = (preOrders) => {/**/
+const preOrderApp = (preOrders) => {/**/
 	/*
 	* The return data is the action, for this action creator.
 	*
@@ -34,3 +26,8 @@ export const preOrderApp = (preOrders) => {/**/
 		}
 	};
 };
+
+export {
+	POST_PREORDER,
+	preOrderApp
+}
