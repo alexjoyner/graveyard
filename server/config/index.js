54c462ced3152a5e3019badb78e259e968bd5434
Rosco <rosco9awj@gmail.com>
2017-05-10 17:47:30 -0400

    now starts the server on init

--- server/config/index.js
+++ server/config/index.js
@@ -7,6 +7,12 @@ function init(app) {
 	app.use(morgan('dev'));
 	/* You MUST set the access headers BEFORE the routes*/
 	app.use(setAccessHeaderMiddleware);
+	let port = (process.env.PORT || 8000);
+	if(!module.parent){
+		app.listen(port, function () {
+			console.log('App running on port ' + port);
+		});
+	}
 }
 
 export {init};
