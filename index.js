17ef77a8dd558c47c704d3261af8c6603e07a6f8
Rosco <rosco9awj@gmail.com>
2017-05-10 17:15:50 -0400

    added automated web pack configuration

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from './utils/addTwoNumbers';
 export * from './server/config';
 export * from './client/test_helper';
 export * from './server/routes';
+export * from './webpack/webpack-dev';
