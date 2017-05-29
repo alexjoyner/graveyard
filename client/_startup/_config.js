caa7da10af538fede6711e7b9d646aa33d37eb16
Rosco <rosco9awj@gmail.com>
2017-05-29 18:49:42 -0400

    Corrected electron deployment

--- client/_startup/_config.js
+++ client/_startup/_config.js
@@ -1,1 +1,5 @@
-export const ROOT_URL = 'http://localhost:8000';
+if(process.env.NODE_ENV === "production"){
+	export const ROOT_URL = 'http://localhost:8000';
+}else{
+	export const ROOT_URL = 'https://cet-database.herokuapp.com/';
+}
