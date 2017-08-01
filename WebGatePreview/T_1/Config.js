ed7263e7db75c2dad0ec3fcd8728c721e56eb161
Alex Joyner <rosco9awj@gmail.com>
2017-08-01 11:02:14 -0400

    Initialized HMI project

---
+++ WebGatePreview/T_1/Config.js
@@ -0,0 +1,21 @@
+
+var config = 
+{ 
+  logo:          "C:\\Program Files (x86)\\Schneider Electric\\Vijeo-Designer 6.2\\Vijeo-Runtime\\Public\\WebGate\\system\\html\\images\\SchneiderLogo.gif",
+  logoPocketPC:  "C:\\Program Files (x86)\\Schneider Electric\\Vijeo-Designer 6.2\\Vijeo-Runtime\\Public\\WebGate\\system\\html\\images\\SchneiderLogoPocketPC.gif",
+  titleHtml:     "Testing",
+  title:         "Vijeo Designer&trade;",
+  titleColor:    "#FFFFFF",
+  titleBgColor:  "#00A64F",
+  homeImage:     "C:\\Program Files (x86)\\Schneider Electric\\Vijeo-Designer 6.2\\Vijeo-Runtime\\Public\\WebGate\\system\\html\\images\\AGP.jpg",
+  homeVersion:   "Website version : 1.0.0.0",
+  homeCopyRight: "Copyright &copy 1998 - 2012, Schneider Electric. All Rights Reserved.", 
+  favIcon:       "C:\\Program Files (x86)\\Schneider Electric\\Vijeo-Designer 6.2\\Vijeo-Runtime\\Public\\WebGate\\system\\favicon.ico"
+}
+
+
+function resizeImg(img) 
+{
+    if(img.height > 60)
+    	img.height = 60;
+}
