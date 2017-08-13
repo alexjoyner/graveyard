4d1d2a806304bfd5f7dfaeff4ec73bcf870e9409
Rosco <rosco9awj@gmail.com>
2017-08-12 23:18:48 -0400

    Running ts over js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import * as P5 from 'p5';
-
-const main = (p) => {
-	let gray = 0;
-
-	p.setup = function () {
-		p.createCanvas(600, 400)
-	};
-
-	p.draw = function () {
-		p.background(gray);
-		p.rect(p.width/2 - 25, p.height/2 - 25, 50, 50)
-	};
-
-	p.mousePressed = function () {
-		gray = (gray + 16) % 256
-	}
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var P5 = require("p5");
+var main = function (p) {
+    var gray = 0;
+    p.setup = function () {
+        p.createCanvas(400, 400);
+    };
+    p.draw = function () {
+        p.background(gray);
+        p.rect(p.width / 2 - 25, p.height / 2 - 25, 50, 50);
+    };
+    p.mousePressed = function () {
+        gray = (gray + 16) % 256;
+    };
 };
-
 new P5(main);
-
-
-
