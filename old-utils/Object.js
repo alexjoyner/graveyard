27c7fc514e3124cb0a84bdef2fccab70fad14b43
Rosco <rosco9awj@gmail.com>
2017-08-13 21:13:46 -0400

    added p5 utils submodule

--- old-utils/Object.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Phys_Object = (function () {
-    function Phys_Object() {
-    }
-    return Phys_Object;
-}());
-exports.Phys_Object = Phys_Object;
--- old-utils/Phys_Object.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Phys_Object = (function () {
-    function Phys_Object(physProps) {
-        this.physProps = physProps;
-    }
-    return Phys_Object;
-}());
-exports.Phys_Object = Phys_Object;
