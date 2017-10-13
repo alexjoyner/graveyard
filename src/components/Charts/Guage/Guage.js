13e3e97f39fa2c0304026974ac92b388a965921d
Alex Joyner <alex@voicir.com>
2017-10-13 16:28:00 -0400

    Discovered how to properly format the chart

--- src/components/Charts/Guage/Guage.js
+++ src/components/Charts/Guage/Guage.js
@@ -5,12 +5,19 @@ const data = {
   type: 'gauge',
   columns: [['data', 90.4]],
 };
+const gaugeSpecs = {
+  label: {
+    format: value => value,
+    show: true, // to turn off the min/max labels.
+  },
+  units: 'F',
+};
 
 class Guage extends React.Component {
   render() {
     return (
       <div>
-        <C3Chart data={data} />
+        <C3Chart data={data} gauge={gaugeSpecs} size={{ height: 180 }} />
       </div>
     );
   }
