51ca8daaca017c42a449786c2c727bb5fbf10225
Alex Joyner <alex@voicir.com>
2017-10-24 15:07:00 -0400

    Had to change port to 80 for communication between microservices

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const request = require('request');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/greeting', function (req, res) {
-    res.send("Hello charles!")
+app.use(morgan('tiny'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.get('/greeting', function (req, res) {
+    request.get('http://hello-name/ronald', function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            return res.send(body).end();
+        }
+        res.send(error).end();
+    });
+});
+
+app.listen(80, function () {
+    console.log('Api service started!')
 });
--- hello-name/server.js
+++ hello-name/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/:name', function (req, res) {
-    res.send("Hello " + req.params.name + "!")
+app.use(morgan('tiny'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.get('/:name', function (req, res) {
+    res.send("Hello " + req.params.name + "!").end();
+});
+
+app.listen(80, function () {
+    console.log('Hello Name service started!')
 });
