9f19b5502744e88291cd517a4a771b4ed5afb701
Alex Joyner <rosco9awj@gmail.com>
2017-11-03 09:32:36 -0400

    init container ui-service
    used the razzle-basic starter app

--- ui-service/docs/config_base_server_side_project.md
+++
@@ -1,7 +0,0 @@
-Configuring a base project with react server side rendering
-====
-
-1) You will need
- - React - for you components
- - React Dom
-    - for using render to string
--- ui-service/src/client.js
+++ ui-service/src/client.js
@@ -1,13 +1,15 @@
-// Startup point for client side app
+import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home/Home';
-import ContextProvider from './helpers/ContextProvider';
+import { hydrate } from 'react-dom';
 
-const css = new Set(); // CSS for all rendered React components
-const context = { insertCss: (...styles) => styles.forEach(style => css.add(style._getCss()))};
-ReactDOM.hydrate(
-    <ContextProvider context={context}>
-        <Home />
-    </ContextProvider>
-    , document.querySelector('#root'));
+hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
