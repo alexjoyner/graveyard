8887d593369b42dead10e71920319ed256a023ae
Alex Joyner <rosco9awj@gmail.com>
2017-12-12 14:46:26 -0500

    Initialized gift-giver project with first test

---
+++ gift-giver/src/components/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
+
+const app = shallow(<App/>);
+
+it('renders correctly', () => {
+   expect(app).toMatchSnapshot();
+});
+
+it('initializes the state with an empty list of gifts', () => {
+});
