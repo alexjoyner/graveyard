f6fbd656a8d143123975993417ebfa642d2987e7
Alex Joyner <rosco9awj@gmail.com>
2017-12-13 08:45:01 -0500

    Init base loot check project

---
+++ loot-check/src/components/App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render(){
+        return (
+            <div><h2>Loot Check</h2></div>
+        )
+    }
+}
