c28484765dfe827b004e534eef4965528c0d1163
Alex Joyner <rosco9awj@gmail.com>
2017-12-13 14:38:42 -0500

    refactored Wallet component into a feature first structure with a balance `duck` folder for redux needs

--- loot-check/src/index.js
+++ loot-check/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import rootReducer from './ducks/reducer';
+import rootReducer from './components/Wallet/balance/reducer';
 import {Provider} from 'react-redux';
 import App from "./components/App";
 
