24a1ad3f59d733c8c16d447b7e6243e88edb042f
Alex Joyner <alex@voicir.com>
2018-05-18 14:50:36 -0400

    started simple test page to show that the component library imports!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Button} from 'ro-component-library/lib/atoms/buttons/buttons';
+import {Panel} from 'ro-component-library/lib/atoms/panel/panel';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Panel>
+          <Button >Test Button</Button>
+        </Panel>
       </div>
     );
   }
