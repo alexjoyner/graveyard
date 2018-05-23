0b6f0da114f98ba76d32f16547358d2675ff4f28
Alex Joyner <alex@voicir.com>
2018-05-23 08:02:23 -0400

    injext global styles to eliminate all css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import "./Global.css";
 import {Header, colors} from 'ro-component-library';
 import {GaugeBlockArray} from './organisms/GaugeBlockArray';
+import {injectGlobal} from 'styled-components';
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:100');
+  body {
+    padding: 0;
+    margin: 0;
+    margin-top: 80px;
+    font-family: 'Roboto', 'sans-serif';
+    background-color: bisque;
+  }
+`
 
 class App extends Component {
   render() {
--- src/organisms/GaugeBlockArray/index.js
+++ src/organisms/GaugeBlockArray/index.js
@@ -1,23 +1,33 @@
 import React, {Component} from 'react';
 import {GaugeBlock, CenteredContent} from 'ro-component-library';
-import {gaugeSettings} from "ro-component-library/src/atoms/Graph/graph-settings/gauge";
+import {gaugeSettings} from "ro-component-library/lib/atoms/Graph/graph-settings/gauge";
 
 class GaugeBlockArray extends Component{
     state = {
         inputs: {}
     }
-    componentDidMount(){
-        let requestUrl = new Request('http://eze-handler.datalogger-api.53243324.svc.dockerapp.io/api/status');
+    retreiveData(passCode, callback){
+        let requestUrl = new Request('https://api.voicir.com');
         fetch(requestUrl)
         .then((response) => response.json())
         .then((myJson) => {
             if(myJson.status === 'Offline')
                 return alert('System offline')
-            this.setState({
-                'inputs': myJson.inputs
-            })
-            this.forceUpdate();
+            callback(myJson.inputs);
+            setTimeout(() => {
+            this.retreiveData(passCode, callback);
+            }, 5000);
         });
+    }
+    componentDidMount(){
+        if(1){
+            this.retreiveData('', (data) => {
+                this.setState({
+                    'inputs': data
+                })
+                this.forceUpdate();
+            });
+        }
     }
     render(){
         return (
