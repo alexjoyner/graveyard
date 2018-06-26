70c387993647dd1df926d0abeee083547403ed65
Rosco <rosco@Roscos-MacBook-Pro-2.local>
2018-06-26 12:40:47 -0400

    server gets historical data from server

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(myLogger);
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/status/:username/:pass', (req, res) => {
+    // Using the request library for it's digest auth function
+    //  notice the sendImmediately equalling 'false'
     request.get('https://ezecontrol.com/api/status.php', {
         'auth': {
             'user': req.params.username,
@@ -27,6 +29,21 @@ app.get('/status/:username/:pass', (req, res) => {
         res.send(body);
     });
 })
+
+app.get('/history/:username/:pass', (req, res) => {
+    const { query, params } = req;
+    const route = `https://ezecontrol.com/api/log.php?input=${query.input}&from=${query.from}&to=${query.to}`
+    request.get(route, {
+        'auth': {
+            'user': params.username,
+            'pass': params.pass,
+            'sendImmediately': false
+        }
+    }, (err, response, body) => {
+        if(err) return res.status(400).send({'Error: ': err});
+        res.send(body);
+    });
+});
 
 http.createServer(app).listen(80, () => console.log('HTTP listening on port 80'));
 //https.createServer(options, app).listen(443, () => console.log('HTTP listening on port 443'));
