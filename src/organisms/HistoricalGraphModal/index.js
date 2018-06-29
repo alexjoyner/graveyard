768461f277c02f9017a1671977ef67d485e3a6c9
Alex Joyner <alex@voicir.com>
2018-06-28 23:07:30 -0400

    changed HistoricalModal to use RoHighChart

--- src/organisms/HistoricalGraphModal/index.js
+++ src/organisms/HistoricalGraphModal/index.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
-import HighCharts from 'highcharts';
-import boost from 'highcharts/modules/boost';
-import HighchartsReact from 'highcharts-react-official'; // eslint-disable-line import/first
 import { connect } from 'react-redux';
 import { RoHighChart, Button, Modal } from 'ro-component-library';
-import { env } from '../../.env';
 import { getChartConfig } from './utils/getChartConfig';
-import { THEME } from 'ro-component-library/lib/atoms/RoHighChart/demoData/theme';
-
-
-boost(HighCharts);
-HighCharts.setOptions(THEME);
 
 class HistoricalGraphModal extends Component {
     constructor(props){
@@ -24,16 +15,12 @@ class HistoricalGraphModal extends Component {
       console.timeEnd('chart-render');
     }
     render() {
-      console.log('Props: ', this.props)
         return (this.props.modalShown)?(
             <Modal>
                 <Button small onClick={() => this.props.dispatch({
                     type: 'HIDE_HISTORICAL_MODAL'
                 })}>X</Button>
-                {/* <RoHighChart config={this.state.config} /> */}
-                <HighchartsReact 
-                  highcharts={HighCharts}
-                  options={this.state.config}/>
+                <RoHighChart config={this.state.config} />
             </Modal>
         ):(<div style={{visibility: 'hidden'}}></div>);
     }
