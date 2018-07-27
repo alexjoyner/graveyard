34105dade83f14978efd9a156ec7f2bb5f016803
alexjoyner <rosco9awj@gmail.com>
2018-07-27 12:23:26 -0400

    init with simple logger that logs every minute

---
+++ index.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const moment = require('moment');
+
+const MAX_VAL = 100;
+const MIN_VAL = 0;
+
+const getRandomValue = (max, min) => {
+    const RAND_VAL = Math.floor(Math.random() * max) + min;
+    return RAND_VAL
+}
+
+const getLog = (opts) => {
+    const RAND_VAL = getRandomValue(opts.max, opts.min);
+    return {
+        metric: opts.metric.id,
+        value: RAND_VAL,
+        timeStamp: moment().format('YYYY-MM-DD HH:mm')
+    }
+}
+
+class Logger{
+    constructor(opts){
+        this.frequency = opts.frequency || 1000; //every 1 second
+        this.max = opts.max || MAX_VAL || 100;
+        this.min = opts.min || MIN_VAL || 0;
+    }
+    run(){
+       const log = getLog({
+        max: this.max,
+        min: this.min,
+        metric: {
+            id: 1
+        }
+       }); 
+       console.log('New Log: ', log);
+       setTimeout(() => {
+           this.run();
+       }, this.frequency);
+    }
+}
+const mainLogger = new Logger({
+    frequency: 1000 * 60
+});
+console.log('Starting logger');
+mainLogger.run();
