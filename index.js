2adec7a3fa54c2921c36e9fba91fce7cd7977677
alexjoyner <rosco9awj@gmail.com>
2018-09-15 16:46:53 -0400

    added route to get all historical data for 1 point

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let morgan = require('morgan');
 let bodyParser = require('body-parser');
+let fs = require('fs');
 const { Client } = require('pg');
 
 const PORT = process.env.PORT || 8080;
@@ -10,23 +11,64 @@ let app = express();
 app.use(bodyParser.json());
 
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use(morgan('common'));
 
+
+app.get('/all/:pointID', async (req, res) => {
+    const { body } = req;
+    try {
+        const conInfo = {
+            user: fs.readFileSync(process.env.PG_USER_FILE, 'utf8'),
+            password: fs.readFileSync(process.env.PG_PASS_FILE, 'utf8'),
+            database: fs.readFileSync(process.env.PG_DB_FILE, 'utf8'),
+            host: fs.readFileSync(process.env.PG_HOST_FILE, 'utf8'),
+        }
+        console.log('Connection Info: ', conInfo)
+        const client = new Client(conInfo);
+        await client.connect();
+        const logs = await client.query({
+            rowMode: 'array',
+            text: `
+            SELECT 
+                date_part('epoch', datetime) * 1000 as time, 
+                val as value
+            FROM 
+                log 
+            WHERE 
+                point_id = $1
+            ORDER BY datetime ASC;`,
+            values: [req.params.pointID]
+        })
+        await client.end();
+        res.send({
+            source: {
+                inputname: 'Test Point',
+                unit: 'F'
+            },
+            data: logs.rows
+        });
+    }
+    catch (e) {
+        console.log("Error: ", e);
+        res.status(500).send('Something went wrong. Sorry');
+    }
+});
+
 app.get('/healthz', function (req, res) {
-	// do app logic here to determine if app is truly healthy
-	// you should return 200 if healthy, and anything else will fail
-	// if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
-  res.send('I am happy and healthy\n');
+    // do app logic here to determine if app is truly healthy
+    // you should return 200 if healthy, and anything else will fail
+    // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
+    res.send('I am happy and healthy\n');
 });
 
 let server = app.listen(PORT, function () {
-  console.log('Log Handler is ready');
+    console.log('Historical Service is ready');
 });
 
 
@@ -41,26 +83,26 @@ let server = app.listen(PORT, function () {
 //
 
 // quit on ctrl-c when running docker in terminal
-process.on('SIGINT', function onSigint () {
-	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
-  shutdown();
+process.on('SIGINT', function onSigint() {
+    console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+    shutdown();
 });
 
 // quit properly on docker stop
-process.on('SIGTERM', function onSigterm () {
-  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
-  shutdown();
+process.on('SIGTERM', function onSigterm() {
+    console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+    shutdown();
 })
 
 // shut down server
 function shutdown() {
-  server.close(function onServerClosed (err) {
-    if (err) {
-      console.error(err);
-      process.exitCode = 1;
-		}
-		process.exit();
-  })
+    server.close(function onServerClosed(err) {
+        if (err) {
+            console.error(err);
+            process.exitCode = 1;
+        }
+        process.exit();
+    })
 }
 //
 // need above in docker container to properly exit
