b06da30a8c0920c013dde14cd1a1da8ffc9e11e5
alexjoyner <rosco9awj@gmail.com>
2018-09-18 15:04:35 -0400

    modified query string to have inputs based off keys, not arrays

--- utils/query_builder/index.js
+++ utils/query_builder/index.js
@@ -1,26 +1,31 @@
 class QueryBuilder {
-    getInsertString(data, tableName = 'log'){
+    getInsertString(data, tableName = 'log') {
         let values = "";
         let logs = data.logs;
-        for(let i = 0; i < logs.length; i++){
-            let lastChar = (i == logs.length - 1)?';':',';
-            values = values + `(${logs[i].point_id}, '${data.datetime}', ${logs[i].value})${lastChar}`
-        }
+        Object.keys(logs).map((point_id /* Point ID is the key */, i, arr) => {
+            let lastChar = (arr.length - 1 === i) ? ';' : ',';
+            values = values + `(${point_id}, '${data.datetime}', ${logs[point_id].value})${lastChar}`;
+        })
         return `INSERT INTO "${tableName}" ("point_id", "datetime", "val") VALUES ${values}`
     }
 }
 
-if(require.main === module){
+if (require.main === module) {
     const test = new QueryBuilder()
     const testData = {
-        datetime: '2018-09-16 03:38:14+00',
-        logs: [{
-            value: 25.4,
-            point_id: 2
-        },{
-            value: 75,
-            point_id: 1
-        }]
+        "datetime": "2018-09-16 03:38:14+00",
+        "logs": {
+            "1": {
+                "name": "Temp Sensor 1",
+                "unit": "F",
+                "value": 24 
+            },
+            "2": {
+                "name": "Temp Sensor 2",
+                "unit": "F",
+                "value": 74.5
+            }
+        }
     }
     console.log(test.getInsertString(testData, 'tmp'));
 }
