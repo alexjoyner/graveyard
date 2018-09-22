057749cae0610a65d9064a98056ba2844c1a02bf
alexjoyner <rosco9awj@gmail.com>
2018-09-22 15:00:26 -0400

    added tables and test data for point groups

--- server-setup.sql
+++ server-setup.sql
@@ -4,6 +4,18 @@ CREATE TABLE client (
 	id SERIAL PRIMARY KEY
 );
 
+CREATE TABLE point_group (
+  id SERIAL PRIMARY KEY,
+  client_id integer NOT NULL REFERENCES client(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  name character varying(20) NOT NULL,
+);
+
+CREATE TABLE point_group_x_point (
+  point_group_id INTEGER,
+  point_id INTEGER,
+  CONSTRAINT point_group_x_point_pkey PRIMARY KEY (point_group_id, point_id)
+);
+
 CREATE TABLE point (
   id SERIAL PRIMARY KEY,
   name character varying(20) NOT NULL,
@@ -30,5 +42,27 @@ INSERT INTO "public"."point"("name", "client_id") VALUES(
   1
 ),(
   'Test Sensor 3',
+  1
+);
+
+INSERT INTO "public"."point_group"("name", "client_id") VALUES(
+  'Predict Maintenance',
+  1
+),(
+  'Production',
+  1
+),(
+  'Process',
+  1
+);
+
+INSERT INTO "public"."point_group_x_point"("point_id", "point_group_id") VALUES(
+  1,
+  1
+),(
+  2,
+  1
+),(
+  3,
   1
 );
