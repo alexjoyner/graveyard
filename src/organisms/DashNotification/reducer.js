cef329b20f2e7ec9d8670bba83aa1063a729b3e0
Alex Joyner <alex@voicir.com>
2018-09-26 18:15:45 -0400

    updated feedback button

--- src/organisms/DashNotification/reducer.js
+++ src/organisms/DashNotification/reducer.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { SHOW_NOTIFICATION, HIDE_NOTIFICATION, START_LOADING, STOP_LOADING } from './actions';
 
 const INITIAL_STATE = {
-    shown: false,
-    contents: <div/>
+  shown: false,
+  contents: <div />
 }
 
 export const NotificationReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type){
-        case SHOW_NOTIFICATION:
-            return {...state, shown: true, contents: <h3>{action.data}</h3>};
-        case START_LOADING:
-            return {...state, shown: true, contents: <h1>Loading</h1>};
-        case STOP_LOADING:
-        case HIDE_NOTIFICATION:
-            return {...state, shown: false};
-        default: return state;
-    }
+  switch (action.type) {
+    case SHOW_NOTIFICATION:
+      return { ...state, shown: true, contents: <h3>{action.data}</h3> };
+    case START_LOADING:
+      return { ...state, shown: true, contents: <h1>Loading</h1> };
+    case STOP_LOADING:
+    case HIDE_NOTIFICATION:
+      return { ...state, shown: false };
+    default: return state;
+  }
 }
--- src/organisms/FeedbackButton/index.js
+++ src/organisms/FeedbackButton/index.js
@@ -1,32 +1,25 @@
-import React, {Component} from 'react';
-import {Button} from 'ro-component-library';
-const Canny = window.Canny;
-class FeedbackButton extends Component{
-  goToFeedback(){
-    Canny('identify', {
-      appID: '5b9fe8c8c2bd95615ee7dbfa',
-      user: {
-        // Replace these values with the current user's data
-        email: 'rosco9awj@gmail.com',
-        name: 'Rosco P. Coltrane',
-        id: 'x3215awesomesauce'
-      },
-    });
-    window.location.href='http://feedback.voicir.com'
+import React, { Component } from 'react';
+import { Button } from 'ro-component-library';
+
+class FeedbackButton extends Component {
+  goToFeedback() {
+    window.location.href = 'http://feedback.voicir.com'
   }
-  render(){
+  render() {
     return (
-      <Button 
-      large 
-      primary 
-      style={{
-        'position': 'absolute',
-        'right': '3px',
-        'marginTop': '3px'
-      }}
-      onClick={() => this.goToFeedback()}>Feedback</Button>
+      <Button
+        large
+        primary
+        style={{
+          'position': 'absolute',
+          'right': '3px',
+          'marginTop': '3px'
+        }}
+        onClick={() => this.goToFeedback()}>
+        Feedback
+      </Button>
     )
   }
 }
 
-export {FeedbackButton};
+export { FeedbackButton };
