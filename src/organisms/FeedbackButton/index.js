4a89cb7b807f6683e668a035c24da64c7dd663e3
Alex Joyner <alex@voicir.com>
2018-09-27 17:03:07 -0400

    changed feedback button to dumb component

--- src/organisms/FeedbackButton/index.js
+++ src/organisms/FeedbackButton/index.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
 import { Button } from 'ro-component-library';
 
-class FeedbackButton extends Component {
-  goToFeedback() {
-    window.location.href = 'http://feedback.voicir.com'
-  }
-  render() {
-    return (
-      <Button
-        size="large"
-        color="primary"
-        style={{
-          'position': 'absolute',
-          'right': '3px',
-          'marginTop': '3px'
-        }}
-        onClick={() => this.goToFeedback()}>
-        Feedback
-      </Button>
-    )
-  }
-}
+const FeedbackButton = () => (
+  <Button
+    size="large"
+    color="primary"
+    style={{
+      'position': 'absolute',
+      'right': '3px',
+      'marginTop': '3px'
+    }}
+    onClick={() => window.location.href = 'http://feedback.voicir.com'}>
+    Feedback
+  </Button>
+)
 
 export { FeedbackButton };
