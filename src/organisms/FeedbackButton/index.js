a12a47412225d8712eb5190b322da73dcae45ecc
Alex Joyner <alex@voicir.com>
2018-10-04 10:43:15 -0400

    fixed placement of feedback button

--- src/organisms/FeedbackButton/index.js
+++ src/organisms/FeedbackButton/index.js
@@ -8,7 +8,7 @@ const FeedbackButton = () => (
     style={{
       'position': 'absolute',
       'right': '3px',
-      'marginTop': '3px'
+      'marginTop': '8px'
     }}
     onClick={() => window.location.href = 'http://feedback.voicir.com'}>
     Feedback
