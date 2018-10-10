f6f972392af352c8027f9d315ea8ea97c0aa88f1
Alex Joyner <alex@voicir.com>
2018-10-10 17:16:12 -0400

    removed catch block from getAllPoints

--- src/organisms/GaugeBlockArray/utils/getAllPoints/index.js
+++ src/organisms/GaugeBlockArray/utils/getAllPoints/index.js
@@ -1,10 +1,5 @@
 import { getPointsFromGroupID } from '../getPointsFromGroupID';
 
-export const getAllPoints = async () => {
-  try {
-    // Group ID 0 is used as an id to get all points
-    return await getPointsFromGroupID(0);
-  } catch (e) {
-    throw new Error(e);
-  }
-};
+// Group ID 0 is used as an id to get all points
+export const getAllPoints = async () =>
+  getPointsFromGroupID(0);
