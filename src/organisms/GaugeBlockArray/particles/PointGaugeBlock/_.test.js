bf2d2abf02fec260c393757a433b4cf424906cdc
Alex Joyner <alex@voicir.com>
2018-10-10 16:50:45 -0400

    add testing for GaugeBlock button click actions

--- src/organisms/GaugeBlockArray/particles/PointGaugeBlock/_.test.js
+++ src/organisms/GaugeBlockArray/particles/PointGaugeBlock/_.test.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { BasePointGaugeBlock } from './';
 
+jest.useFakeTimers();
 describe('PointGaugeBlock component', () => {
   let component;
-  beforeEach(() => {
-    const props = {
-      point: {
-        name: 'test',
-        value: 0,
-        multiselected: false,
-        settings: {
-          units: 'T',
+  let props;
+  describe('shallow mounted version', () => {
+    beforeEach(() => {
+      const props = {
+        point: {
+          name: 'test',
+          value: 0,
+          multiselected: false,
+          settings: {
+            units: 'T',
+          },
         },
-      },
-    };
-    component = shallow(<BasePointGaugeBlock {...props} />);
+      };
+      component = shallow(<BasePointGaugeBlock {...props} />);
+    });
+    it('Should render without exploding', () => {
+      expect(component).toBeDefined();
+    });
+    it('Should match snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
   });
-  it('Should render without exploding', () => {
-    expect(component).toBeDefined();
-  });
-  it('Should match snapshot', () => {
-    expect(component).toMatchSnapshot();
+  describe('full mounted version', () => {
+    beforeEach(() => {
+      props = {
+        multiSelectPoint: jest.fn(),
+        multiDeselectPoint: jest.fn(),
+        id: 1,
+        point: {
+          name: 'test',
+          value: 0,
+          multiselected: false,
+          settings: {
+            units: 'T',
+          },
+        },
+      };
+      component = mount(<BasePointGaugeBlock {...props} />);
+    });
+    it('should contain a GagueBlock', () => {
+      const GaugeBlock = component.find('GaugeBlock');
+      expect(GaugeBlock).toHaveLength(1);
+    });
+    it('should allow clicking chart button', () => {
+      const GaugeBlock = component.find('GaugeBlock');
+      GaugeBlock.props().onChartClick();
+      expect(component.props().multiSelectPoint)
+        .toHaveBeenLastCalledWith(props.point, props.id);
+      // point is deselected after setTimeout
+      jest.runAllTimers();
+      expect(component.props().multiDeselectPoint)
+        .toHaveBeenLastCalledWith(props.id);
+    });
+    it('shouldnt allow clicking settings button', () => {
+      const GaugeBlock = component.find('GaugeBlock');
+      expect(GaugeBlock.props().onSettingsClick)
+        .toThrowError(new Error('No on settings click implementation'));
+    })
   });
 });
