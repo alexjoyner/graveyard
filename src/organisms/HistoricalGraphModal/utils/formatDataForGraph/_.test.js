aa66c266f11d179a76f605c2b8eda38de6258f26
Alex Joyner <alex@voicir.com>
2018-10-10 09:32:50 -0400

    added tests for format data for graph

---
+++ src/organisms/HistoricalGraphModal/utils/formatDataForGraph/_.test.js
@@ -0,0 +1,14 @@
+import { formatDataForGraph } from './';
+
+describe('formatDataForGraph', () => {
+  it('should return correct value', () => {
+    const test = formatDataForGraph(
+      [[1, 2], [3, 4]],
+      [{ id: 1, name: 'test' }, { id: 2, name: 'test2' }],
+    );
+    expect(test).toEqual([
+      { id: 1, name: 'test', data: [1, 2] },
+      { id: 2, name: 'test2', data: [3, 4] },
+    ]);
+  });
+});
