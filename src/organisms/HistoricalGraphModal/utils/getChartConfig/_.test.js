6b23ea603631ab150943fb8e7856c3bf89afc970
Alex Joyner <alex@voicir.com>
2018-10-10 10:15:59 -0400

    added snapshot test for getChartConfig

---
+++ src/organisms/HistoricalGraphModal/utils/getChartConfig/_.test.js
@@ -0,0 +1,18 @@
+import { getChartConfig } from './';
+
+describe('getChartCOnfig', () => {
+  let result;
+  beforeEach(() => {
+    result = getChartConfig([
+      {
+        id: 1, name: 'test', settings: { units: 'T' }, data: [1, 2],
+      },
+      {
+        id: 2, name: 'test2', settings: { units: 'T' }, data: [3, 4],
+      },
+    ]);
+  });
+  it('should match snapshot', () => {
+    expect(result).toMatchSnapshot();
+  });
+});
