f9b0b58667b1d70189cbc1ec8b53b8efde0ff63e
Alex Joyner <alex@voicir.com>
2018-10-11 11:42:17 -0400

    added tests for HistoricalGraphModal

--- src/organisms/HistoricalGraphModal/_.test.js
+++ src/organisms/HistoricalGraphModal/_.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { BaseHistoricalGraphModal } from './';
 
 describe('BaseHistoricalGraphModal component', () => {
@@ -12,7 +12,7 @@ describe('BaseHistoricalGraphModal component', () => {
         dispatchNewHistoricalData: jest.fn(),
         showHistoryModal: jest.fn(),
         closeHistoryModal: jest.fn(),
-        modalStage: 'hidden',
+        modalStage: 'CLOSED',
         chartPoints: {},
       };
       component = shallow(<BaseHistoricalGraphModal {...props} />);
@@ -22,6 +22,72 @@ describe('BaseHistoricalGraphModal component', () => {
     });
     it('Should match snapshot', () => {
       expect(component).toMatchSnapshot();
+    });
+  });
+  describe('Shown Chart Component', () => {
+    let component;
+    beforeEach(() => {
+      const props = {
+        startLoadingNotif: jest.fn(),
+        stopLoadingNotif: jest.fn(),
+        dispatchNewHistoricalData: jest.fn(),
+        showHistoryModal: jest.fn(),
+        closeHistoryModal: jest.fn(),
+        modalStage: 'SHOWN',
+        chartPoints: {},
+      };
+      component = shallow(<BaseHistoricalGraphModal {...props} />);
+    });
+    it('Should render without exploding', () => {
+      expect(component).toBeDefined();
+    });
+    it('Should match snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+  describe('Full Mounted Component', () => {
+    let props;
+    let spy;
+    beforeEach(() => {
+      props = {
+        startLoadingNotif: jest.fn(),
+        stopLoadingNotif: jest.fn(),
+        dispatchNewHistoricalData: jest.fn(),
+        showHistoryModal: jest.fn(),
+        closeHistoryModal: jest.fn(),
+        modalStage: 'SHOWN',
+        chartPoints: {},
+      };
+    });
+    afterEach(() => {
+      if (spy) {
+        spy.mockClear();
+      }
+    });
+    it('Should render without exploding', () => {
+      const component = mount(<BaseHistoricalGraphModal {...props} />);
+      expect(component).toBeDefined();
+    });
+    it('Should call component did update', () => {
+      spy = jest.spyOn(BaseHistoricalGraphModal.prototype, 'componentDidUpdate');
+      const component = mount(<BaseHistoricalGraphModal {...props} />);
+      component.setProps({ ...props, modalStage: 'TESTING' });
+      expect(spy).toHaveBeenCalled();
+    });
+    it('Should call runBuildGraphProcess', () => {
+      spy = jest.spyOn(BaseHistoricalGraphModal.prototype, 'runBuildGraphProcess');
+      const component = mount(<BaseHistoricalGraphModal {...props} />);
+      component.setProps({ ...props, modalStage: 'BUILDING' });
+      expect(spy).toHaveBeenCalled();
+    });
+    it('Should call runBuildGraphProcess from IntervalButton', () => {
+      spy = jest.spyOn(BaseHistoricalGraphModal.prototype, 'runBuildGraphProcess');
+      const component = mount(<BaseHistoricalGraphModal {...props} />);
+      const IntervalButton = component.find('IntervalButton');
+      IntervalButton.forEach((Button) => {
+        Button.props().onClick();
+      });
+      expect(spy).toHaveBeenCalledTimes(5);
     });
   });
 });
