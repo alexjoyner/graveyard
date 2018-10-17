c96544e60119012c1db1619f5684b81d17c3e15b
Alex Joyner <alex@voicir.com>
2018-10-17 13:54:52 -0400

    upgraded project to react-scripts v2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import 'ro-component-library/src/styles/normalize.css';
+import 'ro-component-library/src/styles/typography.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { reducers } from './reducers';
--- src/organisms/MultiSelectedChartsMenu/index.js
+++ src/organisms/MultiSelectedChartsMenu/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { ListItem, ListHeader, CenteredContent, Button, colors } from 'ro-component-library';
-import { GoTrashcan } from 'react-icons/lib/go';
+import { GoTrashcan } from 'react-icons/go';
 import { removePoint } from './actions';
 import { Points } from '../../propTypes';
 import { buildGraph } from '../HistoricalGraphModal/actions';
