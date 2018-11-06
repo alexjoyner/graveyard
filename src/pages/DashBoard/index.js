9c54a765405cfc494b02c58ebbafeed1764b6f6d
alexjoyner <rosco9awj@gmail.com>
2018-11-06 11:34:41 -0500

    updated to babel v7

--- src/pages/DashBoard/index.js
+++ src/pages/DashBoard/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ISideBar, BasicSideBar, BasicSideBarPageBody } from 'ro-component-library';
 
-console.log(BasicSideBar);
 export const DashBoard = () => (
   <ISideBar {...{
     SideBar: BasicSideBar,
