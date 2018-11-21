b695241e1ddf714634c517b29f7b35acc913a505
alexjoyner <rosco9awj@gmail.com>
2018-11-21 14:44:04 -0500

    added /new api route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const QueryBuilder = require('./utils/query_builder');
 const app = utils.getExpressApp('basic');
 const myQueryBuilder = new QueryBuilder();
 
-app.post('/test', async (req, res, next) => {
+const PostLiveLog = async (req, res, next) => {
   const { body } = req;
   try{
     await axios.post('http://socket-service/newlog', body.logs);
@@ -14,7 +14,13 @@ app.post('/test', async (req, res, next) => {
     console.log("Error: ", e);
     res.status(500).send('Something went wrong. Sorry');
   }
-}, utils.runQuery('pg', myQueryBuilder.getInsertString));
+};
+
+// depreciating /log/test to move to /log/new
+//   - Both do the same thing, just a naming changes
+//      v2 should remove /test
+app.post('/test', PostLiveLog, utils.runQuery('pg', myQueryBuilder.getInsertString));
+app.post('/new', PostLiveLog, utils.runQuery('pg', myQueryBuilder.getInsertString));
 
 app.post('/', function (req, res) {
   console.log('New Log: ', req.body);
