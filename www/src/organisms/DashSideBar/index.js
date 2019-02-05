f8be1ecee260341636c68980581d53876b885d23
Alex Joyner <alex@voicir.com>
2019-02-05 13:03:25 -0500

    removed DashSideBar organism

--- www/src/organisms/DashSideBar/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { AnimatedSideBar } from 'ro-component-library';
-import { SideBarContent } from '../SideBarContent';
-
-export const DashSideBar = props => (
-  <SideBarContent />
-);
