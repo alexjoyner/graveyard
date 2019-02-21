80959c8644ebdb55e030cfaefec23d8e4200fd01
Alex Joyner <alex@voicir.com>
2019-02-21 10:37:59 -0500

    Removed charts selected from SideBar

--- www/src/organisms/SideBar/SideBarContent/index.js
+++ www/src/organisms/SideBar/SideBarContent/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { GroupsMenu } from '../../../molecules/GroupsMenu';
-import { ChartsSelected } from '../../../molecules/ChartsSelected';
 
 const SideBarContent = props => (
   <>
     <GroupsMenu {...props} />
-    <ChartsSelected />
   </>
 );
 
