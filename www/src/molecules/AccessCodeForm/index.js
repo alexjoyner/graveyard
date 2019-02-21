b3d0081d9d94b80fef5b92ae476d1f4a9954f78e
Alex Joyner <alex@voicir.com>
2019-02-21 00:33:14 -0500

    Extracted Modal dependency from AccessCodeForm

--- www/src/molecules/AccessCodeForm/index.js
+++ www/src/molecules/AccessCodeForm/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Input } from 'ro-component-library/Input';
-import { Modal } from 'ro-component-library/Modal';
 import { Button } from 'ro-component-library/Button';
 import { Block } from 'ro-component-library/utilities/Block';
 import PropTypes from 'prop-types';
@@ -13,39 +12,33 @@ const AccessCodeForm = ({ onSubmit }) => {
   };
   return (
   <>
-    <Modal>
-      {({ StyledBody }) => (
-        <StyledBody>
-          <form
-            onSubmit={handleSubmit}
-            data-testid="code-form"
-            style={{
-              minWidth: '320px',
-            }}
-          >
-            <Block display="flex">
-              <Block width="100%" marginRight="scale400">
-                <Input
-                  id="accessCode"
-                  model="basic"
-                  placeholder="Access Code"
-                  overrides={{ Input: { props: { 'data-testid': 'accessCode' } } }}
-                  autoFocus
-                />
-              </Block>
-              <Button
-                model="baseUI"
-                type="submit"
-                value="Submit"
-                data-testid="submit"
-              >
-                Submit
-              </Button>
-            </Block>
-          </form>
-        </StyledBody>
-      )}
-    </Modal>
+    <form
+      onSubmit={handleSubmit}
+      data-testid="code-form"
+      style={{
+        minWidth: '320px',
+      }}
+    >
+      <Block display="flex">
+        <Block width="100%" marginRight="scale400">
+          <Input
+            id="accessCode"
+            model="basic"
+            placeholder="Please enter Access Code"
+            overrides={{ Input: { props: { 'data-testid': 'accessCode' } } }}
+            autoFocus
+          />
+        </Block>
+        <Button
+          model="baseUI"
+          type="submit"
+          value="Submit"
+          data-testid="submit"
+        >
+          Submit
+        </Button>
+      </Block>
+    </form>
   </>
   );
 };
--- www/src/molecules/AccessCodeForm/stories.js
+++ www/src/molecules/AccessCodeForm/stories.js
@@ -2,14 +2,40 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Card } from 'ro-component-library/Card';
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'ro-component-library/Modal';
+import { Button } from 'ro-component-library/Button';
+import { ModalStateContainer } from '../../utils/ModalStateContainer';
 import { AccessCodeForm } from '.';
 
 
 storiesOf('Molecules', module)
   .add('Access Code Modal', () => (
-    <Card>
-      {() => (
+    <>
+      <Card title="Basic Access Code Form">
         <AccessCodeForm onSubmit={action('Submit')} />
-      )}
-    </Card>
+      </Card>
+      <Card title="Modal Access Code Form">
+        <ModalStateContainer>
+          {({ open, close, isOpen }) => (
+            <>
+              <Button onClick={open}>Sign In!</Button>
+              <Modal onClose={close} isOpen={isOpen}>
+                <ModalHeader>Welcome!</ModalHeader>
+                <ModalBody>
+                  <AccessCodeForm onSubmit={action('Submit')} />
+                </ModalBody>
+                <ModalFooter>
+                  <br />
+                </ModalFooter>
+              </Modal>
+            </>
+          )}
+        </ModalStateContainer>
+      </Card>
+    </>
   ));
