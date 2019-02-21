065a9c417472cecb3b974d332f95e2728f99e095
Alex Joyner <alex@voicir.com>
2019-02-21 10:37:30 -0500

    added SignIn form modal button to Header

--- www/src/organisms/Header/index.js
+++ www/src/organisms/Header/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { GoThreeBars } from 'react-icons/go';
 import { Header as RoHeader, sideBarActions } from 'ro-component-library';
 import { Button } from 'ro-component-library/Button';
-import { FeedbackButton } from '../../molecules/FeedbackButton';
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'ro-component-library/Modal';
+import { ModalStateContainer } from '../../utils/ModalStateContainer';
+import { AccessCodeForm } from '../../molecules/AccessCodeForm';
 import { useWindowSize } from '../../effects/useWindowSize';
 
 const ToggleBtn = props => (
@@ -21,9 +28,35 @@ const Header = () => {
   return (
     <>
       <RoHeader color="dark">
-        {(width > 800) ? null : <ToggleBtn onClick={() => sideBarActions().toggle()} />}
+        {(width > 800) ? null : <ToggleBtn model="classic" onClick={() => sideBarActions().toggle()} />}
         <h1 style={logoStyles}>OEE Master</h1>
-        <FeedbackButton />
+        <ModalStateContainer>
+          {({ open, close, isOpen }) => (
+            <>
+              <Button
+                model="classic"
+                color="primary"
+                size="small"
+                onClick={open}
+                style={{
+                  position: 'absolute',
+                  right: '10px',
+                }}
+              >
+                <span>Sign In!</span>
+              </Button>
+              <Modal onClose={close} isOpen={isOpen}>
+                <ModalHeader>Welcome!</ModalHeader>
+                <ModalBody>
+                  <AccessCodeForm onSubmit={data => console.log('Submit', data)} />
+                </ModalBody>
+                <ModalFooter>
+                  <br />
+                </ModalFooter>
+              </Modal>
+            </>
+          )}
+        </ModalStateContainer>
       </RoHeader>
     </>
   );
