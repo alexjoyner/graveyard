be9871aa57e6331d1d6d34ddfc48970e0e38c918
alexjoyner <rosco9awj@gmail.com>
2019-04-16 14:07:40 -0400

    added tests for gauges

--- src/features/Gauges/gauges.test.tsx
+++ src/features/Gauges/gauges.test.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, FC } from 'react';
 import { FeaturesContext } from '../../shared/AppBuilder/featuresContext';
 import { GaugeBlockProps } from './public/GaugeBlock/GaugeBlock';
-import { RenderResult, render } from 'react-testing-library';
+import { RenderResult, render, fireEvent } from 'react-testing-library';
 import { TestApp } from '../../shared/AppBuilder/test-utils';
 import { GaugesFeature } from './gauges';
 
@@ -41,6 +41,12 @@ describe('Gauges Feature', () => {
   test('Should render the gaugeblock', async () => {
     const { getByText } = utils;
     expect(getByText('Test Gauge')).toBeDefined();
+    expect(getByText('20F')).toBeDefined();
   })
-
+  test('Calls the onChartClick handler', () => {
+    const { getByText } = utils;
+    expect(TestGaugeSettings.onChartClick).toBeCalledTimes(0);
+    fireEvent.click(getByText('Test Gauge'));
+    expect(TestGaugeSettings.onChartClick).toBeCalledTimes(1);
+  });
 })
