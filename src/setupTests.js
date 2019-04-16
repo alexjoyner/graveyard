22271f68ca0b489fe6faa8a746d55650532e2247
alexjoyner <rosco9awj@gmail.com>
2019-04-16 12:08:53 -0400

    Added tests for features feature

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,13 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-localstorage-mock';
 import 'react-testing-library/cleanup-after-each';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+beforeEach(() => {
+  // values stored in tests will also be available in other tests unless you run
+  localStorage.clear();
+  // you could also reset all mocks, but this could impact your other mocks
+  jest.resetAllMocks();
+});
