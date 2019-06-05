d3c679f139e6f22e162a1ec3a668671b18f4d099
alexjoyner <rosco9awj@gmail.com>
2019-06-05 10:51:43 -0400

    updated live data service to lates version of ro-server-util

--- server/services/live-data/index.js
+++ server/services/live-data/index.js
@@ -1,8 +1,8 @@
-const utils = require('ro-server-utils');
+const { makeApp } = require('ro-server-utils');
 const axios = require('axios');
 const NodeCache = require('node-cache');
 
-const app = utils.getExpressApp()('basic');
+const app = makeApp('basic');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -70,4 +70,62 @@ io.on('connection', (socket) => {
 });
 
 
-utils.runExpressApp()(server);
+// runApp(server);
+
+const PORT = process.env.PORT || 8080;
+// #### Global Error handler
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.status || 500);
+  res.json({
+    error: { message: err.message || 'Internal Error' },
+  });
+  return null;
+});
+
+server.listen(PORT, () => {
+  console.log(
+    `App is running on interal port ${PORT}:
+        If this service is running in a container, you
+        will need to expose this port to the outside world
+        to have direct access to it.`,
+  );
+});
+
+//
+// need this in docker container to properly exit since node doesn't handle SIGINT/SIGTERM
+// this also won't work on using npm start since:
+// https://github.com/npm/npm/issues/4603
+// https://github.com/npm/npm/pull/10868
+// https://github.com/RisingStack/kubernetes-graceful-shutdown-example/blob/master/src/index.js
+// if you want to use npm then start with `docker run --init` to help, but I still don't think it's
+// a graceful shutdown of node process
+//
+
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', () => {
+  console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+});
+
+// quit properly on docker stop
+process.on('SIGTERM', () => {
+  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+});
+
+// shut down server
+function shutdown() {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+    }
+    process.exit();
+  });
+}
+//
+// need above in docker container to properly exit
+//
