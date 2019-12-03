5bb310c1a10397e9b428a1e09503254a9993cdfd
Alex Joyner <alex@voicir.com>
2019-12-03 13:37:48 -0500

    changed default cypress test

--- cypress/integration/accounts.spec.js
+++ cypress/integration/accounts.spec.js
@@ -1,8 +1,8 @@
 describe('Accounts Feature', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000');
-  })
-  it('Has defaults', () => {
-    cy.contains('OEE Master');
-  });
-})
+	beforeEach(() => {
+		cy.visit('http://localhost:3000');
+	});
+	it('Has defaults', () => {
+		cy.contains('PROJECT_NAME');
+	});
+});
